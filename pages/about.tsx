8bbf898c8696c7e180459e9fbd9ead851854a483
Toxcastel <a.castelr@gmail.com>
2022-11-21 12:03:48 -0300

    basic changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function About() {
     return (
         <div className={styles.container}>
             <Head>
@@ -13,11 +13,11 @@ export default function Home() {
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Go to <Link href="/about">About</Link>
+                    Go to <Link href="/">Home</Link> 
                 </h1>
 
                 <p className={styles.description}>
-                    Get started by editing <code className={styles.code}>pages/index.tsx</code>
+                    Get started by editing <code className={styles.code}>pages/about.tsx</code>
                 </p>
             </main>
         </div>
